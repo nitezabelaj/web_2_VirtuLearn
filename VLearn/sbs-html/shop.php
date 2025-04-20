0d2e9690cd9371596ed790e09e3f91cfe6eed782
amatraci <149633690+amatraci@users.noreply.github.com>
2025-04-20 18:59:52 +0200

    editim i vogel i search engine

--- VLearn/sbs-html/shop.php
+++ VLearn/sbs-html/shop.php
@@ -244,14 +244,16 @@ $emriFaqes = "VirtuLearn";
                         <a href="Javascript:void(0)" onclick="toggleSearch()">
                            <i class="fa fa-search" style="cursor: pointer;" aria-hidden="true"></i>
                         </a>
+                        </li>
+                        </ul>
                         <form method="GET" id="search-form" style="display: none; position: absolute; top: 30px; right: 0; background: white; padding: 5px; border-radius: 5px; z-index: 100;">
                            <input type="text" name="search" placeholder="Search..." required>
                            <button type="submit" style="border: none; background: none;">
                               <i class="fa fa-arrow-right"></i>
                            </button>
                         </form>
-                     </li>
-                  </ul>
+                     
+                  
                </div>
             </div>
          </div>
