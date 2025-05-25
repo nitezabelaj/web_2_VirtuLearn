11b2591c75a3d218c1ce334fecef8ee74e935854
AmelaSyla <148979964+AmelaSyla@users.noreply.github.com>
2025-05-25 15:52:43 -0700

    ,,

--- VLearn/sbs-html/build_skateboard.php
+++ VLearn/sbs-html/build_skateboard.php
@@ -579,13 +579,98 @@ echo '
 </script>
 ';
 ?>
+<?php
+session_start();
+
+if (!isset($_SESSION['myValue'])) {
+    $_SESSION['myValue'] = "Duke përditësuar këtë fjali, ti mund të japësh info interesante rreth skateboard.";
+}
+
+if (isset($_GET['action'])) {
+    header('Content-Type: application/json');
+    $action = $_GET['action'];
+
+    switch ($action) {
+        case 'read':
+            echo json_encode(['value' => $_SESSION['myValue']]);
+            break;
+
+        case 'update':
+            $newValue = trim($_POST['value'] ?? '');
+            if ($newValue !== '') {
+                $_SESSION['myValue'] = $newValue;
+                echo json_encode(['success' => true, 'newValue' => $_SESSION['myValue']]);
+            } else {
+                echo json_encode(['error' => 'Vlera është bosh']);
+            }
+            break;
+
+        default:
+            echo json_encode(['error' => 'Veprim i pavlefshëm']);
+    }
+    exit;
+}
+?>
+
+<h3 id="display">Duke ngarkuar...</h3>
+
+<input type="text" id="inputValue" placeholder="Shkruaj info rreth skateboard" />
+<button onclick="updateValue()">Përditëso Fjalinë</button>
+
+<div id="message"></div>
+
+<script>
+    function readValue() {
+        fetch('?action=read')
+            .then(res => res.json())
+            .then(data => {
+                if(data.value !== undefined){
+                    document.getElementById('display').innerText = data.value;
+                    document.getElementById('message').innerText = '';
+                } else {
+                    document.getElementById('message').innerText = 'Gabim në lexim: ' + data.error;
+                }
+            })
+            .catch(err => {
+                document.getElementById('message').innerText = 'Gabim në lidhje: ' + err;
+            });
+    }
+
+    function updateValue() {
+        const newVal = document.getElementById('inputValue').value.trim();
+        if(newVal === ''){
+            document.getElementById('message').innerText = 'Fusha është bosh, shkruaj diçka!';
+            return;
+        }
+
+        fetch('?action=update', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: `value=${encodeURIComponent(newVal)}`
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.success){
+                document.getElementById('message').innerText = 'Fjalimi u përditësua me sukses!';
+                readValue();
+                document.getElementById('inputValue').value = '';
+            } else {
+                document.getElementById('message').innerText = 'Gabim në përditësim: ' + data.error;
+            }
+        })
+        .catch(err => {
+            document.getElementById('message').innerText = 'Gabim në lidhje: ' + err;
+        });
+    }
+
+    readValue();
+</script>
 
 
 </body>
 </html>
 
-</body>
-</html>
+
 <?php
 ob_end_flush();  
 ?> 
